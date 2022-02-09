851cb008a3b35f13c688bd3f215ffebc0d7759c3
Karthik Belida <Karthik392001@gmail.com>
2022-02-09 11:48:17 +0530

    Updates

--- src/constants/Constant.js
+++
@@ -1,4 +0,0 @@
-export const LinkeIn="https://www.linkedin.com/in/karthik-belida-5812a61b0/"
-export const Instagram="https://www.instagram.com/karthikbelida/"
-export const GitHub="https://github.com/karthik0309"
-export const AboutMeContent="Hello my name is karthik. I am web developer from hyderabad,India.I love working with variety of technologies,including web developement and mobile developement."
